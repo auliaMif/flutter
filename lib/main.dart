868795cbe1de762eaf91f6c22e25b491a072cc51
FlutLab.io <>
2022-04-06 05:57:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'nav-drawer.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "BelajarFlutter.com",
+    title: "BelajarFlutter",
     home: BelajarNavigationDrawer(),
   ));
 }
@@ -13,11 +13,11 @@ class BelajarNavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Belajar Drawer Nav"),
+        title: Text("Drawer Saya"),
       ),
       drawer: DrawerWidget(),
       endDrawer: DrawerWidget(),
-      body: Center(child: Text('Belajar Navigation Drawer di Flutter', style: TextStyle(fontSize: 20))),
+      body: Center(child: Text('Belajar Navigation Drawer', style: TextStyle(fontSize: 20))),
     );
   }
 }

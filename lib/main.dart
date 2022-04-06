e625026a760ae8d49f7c0b1ec54c4d83d606d7cb
FlutLab.io <>
2022-04-06 14:43:20 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 4,
+        length: 3,
         child: Scaffold(
           appBar: AppBar(
             title: Text('List Anime'),
@@ -22,13 +22,14 @@ class NavigationDrawer extends StatelessWidget {
                 new Tab(icon: new Icon(Icons.folder), text: "My Files"),
                 new Tab(icon: new Icon(Icons.group), text: "Shared with me"),
                 new Tab(icon: new Icon(Icons.access_time), text: "Recent"),
-                new Tab(icon: new Icon(Icons.delete), text: "Trash"),
               ],
             ),
           ),
           drawer: DrawerWidget(),
           body: TabBarView(
             children: <Widget>[
+              Galeri(),
+              Galeri(),
               Galeri(),
             ],
           ),

f4c210b75f9e3e6feb129fb355ae5042481253ce
FlutLab.io <>
2022-04-06 14:33:09 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,35 @@ import 'nav-drawer.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "BelajarFlutter",
-    home: BelajarNavigationDrawer(),
+    debugShowCheckedModeBanner: false,
+    title: "Belajar Flutter",
+    home: NavigationDrawer(),
   ));
 }
 
-class BelajarNavigationDrawer extends StatelessWidget {
+class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: Text("Drawer Saya"),
-            bottom: TabBar(tabs: [
-              new Tab(icon: new Icon(Icons.folder), text: "My Files"),
-              new Tab(icon: new Icon(Icons.group), text: "Shared with me"),
-              new Tab(icon: new Icon(Icons.access_time), text: "Recent"),
-              new Tab(icon: new Icon(Icons.delete), text: "Trash"),
-            ])),
-        drawer: DrawerWidget(),
-        endDrawer: DrawerWidget(),
-        body: TabBarView(children: <Widget>[
-          Galeri(),
-        ]));
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('List Anime'),
+            bottom: TabBar(
+              tabs: [
+                new Tab(icon: new Icon(Icons.folder), text: "My Files"),
+                new Tab(icon: new Icon(Icons.group), text: "Shared with me"),
+                new Tab(icon: new Icon(Icons.access_time), text: "Recent"),
+                new Tab(icon: new Icon(Icons.delete), text: "Trash"),
+              ],
+            ),
+          ),
+          drawer: DrawerWidget(),
+          body: TabBarView(
+            children: <Widget>[
+              Galeri(),
+            ],
+          ),
+        ));
   }
 }

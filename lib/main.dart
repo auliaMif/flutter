fdb8e113c47d076d8941780391b13376dcda384d
FlutLab.io <>
2022-04-06 15:16:51 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           appBar: AppBar(
             title: Text('Ariana Grande'),
@@ -22,6 +22,7 @@ class NavigationDrawer extends StatelessWidget {
                 new Tab(icon: new Icon(Icons.folder), text: "My Files"),
                 new Tab(icon: new Icon(Icons.group), text: "Shared with me"),
                 new Tab(icon: new Icon(Icons.access_time), text: "Recent"),
+                new Tab(icon: new Icon(Icons.delete), text: "Trash"),
               ],
             ),
           ),
@@ -31,6 +32,7 @@ class NavigationDrawer extends StatelessWidget {
               Galeri(),
               Share(),
               Galeri(),
+              Trash(),
             ],
           ),
         ));
--- lib/nav-drawer.dart
+++ lib/nav-drawer.dart
@@ -138,3 +138,27 @@ class Share extends StatelessWidget {
     );
   }
 }
+
+class Trash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Text(
+              "Trash",
+              style: new TextStyle(fontSize: 30.0),
+            ),
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Icon(
+              Icons.delete,
+              size: 90.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

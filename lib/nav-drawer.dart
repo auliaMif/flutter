8bdece5cc55b56fd0bb5445e6b48bc72d0e0b5bb
FlutLab.io <>
2022-04-06 13:39:27 +0000

    Autosave

--- lib/nav-drawer.dart
+++ lib/nav-drawer.dart
@@ -10,9 +10,9 @@ class DrawerWidget extends StatelessWidget {
         children: <Widget>[
           _drawerHeader(),
           _drawerItem(icon: Icons.folder, text: 'My Files', onTap: () => DefaultTabController.of(context)?.animateTo(1)),
-          _drawerItem(icon: Icons.group, text: 'Shared with me', onTap: () => print('Tap Shared menu')),
-          _drawerItem(icon: Icons.access_time, text: 'Recent', onTap: () => print('Tap Recent menu')),
-          _drawerItem(icon: Icons.delete, text: 'Trash', onTap: () => print('Tap Trash menu')),
+          _drawerItem(icon: Icons.group, text: 'Shared with me', onTap: () => DefaultTabController.of(context)?.animateTo(2)),
+          _drawerItem(icon: Icons.access_time, text: 'Recent', onTap: () => DefaultTabController.of(context)?.animateTo(3)),
+          _drawerItem(icon: Icons.delete, text: 'Trash', onTap: () => DefaultTabController.of(context)?.animateTo(4)),
           Divider(height: 25, thickness: 1),
           Padding(
             padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
